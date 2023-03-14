6a6c4f162d826fc7111a1a12ce238ab644cc776a
Erick Marques <erick.marquesc@hotmail.com>
2023-03-14 20:25:39 -0300

    style: :lipstick: add globalstyles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { GlobalStyles } from '@/styles/globals';
+import type { AppProps } from 'next/app';
+
+GlobalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
-}
+};
