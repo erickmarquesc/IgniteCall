7297c8a374adef7bd9406c193b9232e3a107feca
Erick Marques <erick.marquesc@hotmail.com>
2023-03-21 17:49:46 -0300

    feat: :sparkles: created a new component
    componente que é toda a estrutura da pagina home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,35 @@
+import { Heading, Text } from '@ignite-ui/react';
+import { Container, Hero, Preview } from './styled';
+
+/* Estrutura de imagem para next */
+import Image from 'next/image';
+import previewImg from '../../assets/previewAppImg.png';
+import { ClaimUsernameForm } from './ClaimUsernameForm';
+
+/* Quando usamos imagens estruturas (imagens/assets),
+ * é importante que no Next seja utilizado um compornente do next/image,
+ * esse compornente ajuda a renderizar a imagem otimizando-a,
+ */
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading>Agendamento descomplicado</Heading>
+        <Text>
+          Conecte seu calendário e permita que as
+          pessoas marquem agendamentos no seu tempo livre.
+        </Text>
+        <ClaimUsernameForm/>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImg}
+          height={400} /* Altura máxima que a imagem vai esticar */
+          quality={100} /* Por padrão o next reduz a qualidade para 80% então voltei para 100% */
+          priority /* Assim terá prioridade no carregamento não sendo o ultimo elemento a carregar */
+          alt='Calendário simbolizando aplicação em funcionamento'
+        />
+      </Preview>
+    </Container>
+  );
+};
---
+++ src/pages/home/styled.ts
@@ -0,0 +1,45 @@
+import { Heading, styled, Text } from "@ignite-ui/react";
+
+export const Container = styled('div', {
+  gap: '$20',
+  height: '100vh',
+  display: 'flex',
+  marginLeft: 'auto',
+  overflow: 'hidden',
+  alignItems: 'center',
+  maxWidth: 'calc(100vw - (100vw - 1160px)/2)',
+});
+
+export const Hero = styled('div', {
+  maxWidth: 480,
+  padding: '0 $10',
+
+  /* Estilizando componentes */
+  /* Usando o sinal > eu deixo claro que a estilização
+   * é apenas para os componentes de maior nível e não 
+   * os que estão dentro dos componentes
+   */
+  [`>${Heading}`]: {
+    fontSize: '$4xl',
+
+    '@media(max-width:600px)': {
+      fontSize: '$6xl',
+    },
+  },
+
+  [`>${Text}`]:{
+    fontSize: '$lg',
+    marginTop: '$2',
+    color: '$gray200',
+  },
+
+});
+
+export const Preview = styled('div', {
+  paddingRight: '$8',
+  overflow: 'hidden',
+
+  '@media(max-width:600px)': {
+    display: 'none',
+  },
+});
