6866c9973db7d645178d3f66cde009fce9182d7c
Erick Marques <erick.marquesc@hotmail.com>
2023-03-24 17:32:54 -0300

    feat: :sparkles: add existing user check
    foi necessário criar essa validação para evitar que o usuário tente criar uma login com nome de usuário já existente

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -3,14 +3,29 @@ import { prisma } from '@/lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  
+
   if (req.method !== 'POST') {
     return res.status(405).end(); //405 já indica que o metodo ñ pode usar a rota e o end n envia corpo
   };
 
   /* Prisma cria o usuário */
   const { name, username } = req.body;
-  
+
+  /* Procura por um usuário já existente */
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    }
+  });
+
+  if (userExists) {
+    return (
+      res.status(400).json({
+        message: 'Username already taken.',
+      })
+    );
+  };
+
   const user = await prisma.user.create({
     data: {
       name,
