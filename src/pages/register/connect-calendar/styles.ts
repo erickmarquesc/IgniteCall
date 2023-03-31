e2f55c847d36874642966df4de20c37a8e98ef43
Erick Marques <erick.marquesc@hotmail.com>
2023-03-31 17:52:35 -0300

    style: :lipstick: add a new componente and update UI

--- src/pages/register/connect-calendar/styles.ts
+++ src/pages/register/connect-calendar/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from "@ignite-ui/react";
+import { Box, styled, Text } from "@ignite-ui/react";
 
 export const ConnectBox = styled(Box, {
   marginTop: '$6',
@@ -8,10 +8,23 @@ export const ConnectBox = styled(Box, {
 
 export const ConnecItem = styled('div', {
   display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  border: '1px solid $gray600',
   padding: '$4 $6',
+  marginBottom: '$4',
   borderRadius: '$md',
-  marginBottom: '$2',
+  alignItems: 'center',
+  border: '1px solid $gray600',
+  justifyContent: 'space-between',
+});
+
+export const ProfileImg = styled('div', {
+  display: 'flex',
+  borderRadius: "$sm",
+  alignItems: "center",
+  justifyContent: "center",
+  border: '4px solid $ignite500',
+});
+
+export const AuthError = styled(Text, {
+  color: '#f75a68',
+  marginBottom: '$4',
 });
