2f9213e592b76ca044d3feb79e97eb16c14b1708
Erick Marques <erick.marquesc@hotmail.com>
2023-04-12 15:18:45 -0300

    feat: :construction: create a new component

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,35 @@
 import { Calendar } from "@/components/Calendar";
-import { Container } from "./styles";
+import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./styles";
 
 export function CalendarStep() {
+  const isDateSelected = true;
+
   return (
-    <Container>
-      <Calendar/>
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20de setembro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>08:00</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>)}
     </Container>
   );
 };
