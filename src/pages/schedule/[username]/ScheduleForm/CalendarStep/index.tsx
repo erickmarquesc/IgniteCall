9ff8a8389de9bbad130761f42a081647f35d0c42
Erick Marques <erick.marquesc@hotmail.com>
2023-04-13 16:24:18 -0300

    refactor: :zap: add format days

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,17 +1,24 @@
 import { Calendar } from "@/components/Calendar";
 import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./styles";
+import { useState } from "react";
+import dayjs from "dayjs";
 
 export function CalendarStep() {
-  const isDateSelected = true;
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  const isDateSelected = !!selectedDate;
+
+  const selectedWeekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null;
+  const selectedMonthDay = selectedDate ? dayjs(selectedDate).format('DD[ de ]MMMM') : null;
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
-      <Calendar />
+      <Calendar selectedDate={selectedDate} onDeteSelected={setSelectedDate} />
 
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            terça-feira <span>20de setembro</span>
+            {selectedWeekDay} <span>{selectedMonthDay}</span>
           </TimePickerHeader>
 
           <TimePickerList>
